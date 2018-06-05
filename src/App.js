b0c1139dd0be334345cc3d2de1120f35bb4c33f4
Tony Thompson <penguinprofession@icloud.com>
2018-06-05 18:35:16 -0400

    created card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from './cards/card';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Card />
       </div>
     );
   }
--- src/cards/card.jsx
+++ src/cards/card.jsx
@@ -1,2 +1,18 @@
 import React, { Component } from 'react';
-import './card.css'
+import './card.css';
+
+const Card = (props) => (
+    <div className="card-container">
+        <div className="card">
+            <div className="front">
+                <div className="eng">word</div>
+            </div>
+            <div className="back">
+                <div className="han">test</div>
+                <div className="pin">test</div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Card;
